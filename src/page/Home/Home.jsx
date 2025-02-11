22d046b12c8d4fe70a5db2a36f2c8822e34466ce
adel-kd <adelkedir971@gmail.com>
2025-02-11 16:01:33 -0500

    header and footer

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../component/Header/Header";
+import Footer from "../../component/Footer/Footer";
+
+const home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default home;
