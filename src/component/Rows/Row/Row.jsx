ae0fbb166b3c737125df4d10e85340bc9274704b
adel-kd <adelkedir971@gmail.com>
2025-02-19 11:43:25 -0500

    row

---
+++ src/component/Rows/Row/Row.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import "./row.css";
+import axios from "../../../utils/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  const base_url = "https://image.tmdb.org/t/p/original";
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // console.log(fetchUrl);
+        const request = await axios.get(fetchUrl);
+        // console.log(request);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          // console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          console.log(urlParams.get("v"));
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log("Error finding trailer: ", error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row_posters">
+        {movies?.map((movie, index) => {
+          const imageUrl = `${base_url}${
+            isLargeRow ? movie.poster_path : movie.backdrop_path
+          }`;
+          console.log(
+            `Movie: ${movie.name}, Poster Path: ${movie.poster_path}, Backdrop Path: ${movie.backdrop_path}, Image URL: ${imageUrl}`
+          );
+          return (
+            <img
+              onClick={() => handleClick(movie)}
+              key={index}
+              src={imageUrl}
+              alt={movie.name}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            />
+          );
+        })}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Header from "../../component/Header/Header";
 import Footer from "../../component/Footer/Footer";
 import Banner from "../../component/Banner/Banner";
+import RowList from "../../component/Rows/RowList/rowlist";
 
-const home = () => {
+const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
 };
 
-export default home;
+export default Home;
