06417928fa210d494437b0e72955ff185774ad90
adel-kd <adelkedir971@gmail.com>
2025-04-11 03:34:01 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Home from "./page/home/home";
+import Home from "./page/Home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
